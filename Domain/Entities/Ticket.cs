ea5db258f0dd07fea1f1b3cb8b05b42c9419fb71
alvaroramirezbrusco <alvaroramirezbrusco@gmail.com>
2025-11-18 11:40:50 -0300

    Cambio de version de paquetes y entidades de ticket

---
+++ Domain/Entities/Ticket.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class Ticket
+    {
+        public Guid TicketId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid EventId { get; set; }
+        public Guid OrderDetailId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+
+        // Relationships
+        public TicketStatus StatusRef { get; set; }
+    }
+}
---
+++ Domain/Entities/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class TicketStatus
+    {
+        public int StatusID { get; set; }
+        public string Name { get; set; }
+
+        // Relashionsships
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
